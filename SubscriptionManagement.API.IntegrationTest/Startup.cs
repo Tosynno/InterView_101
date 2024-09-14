b7993d94bcb9ff91833ca1ecb0a0edab4f49e99b
Tosin Raphael <tosin.olowogboye@sterling.ng>
2024-09-14 10:23:53 +0100

    Startup added

---
+++ SubscriptionManagement.API.IntegrationTest/Startup.cs
@@ -0,0 +1,19 @@
+﻿using SubscriptionManagement.Infrastructure.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionManagement.API.IntegrationTest
+{
+    [Binding]
+    internal class Startup
+    {
+        [BeforeTestRun]
+        public static void Setup()
+        {
+            SubscriptionManagementDataContext.Connectionstring = "Server=.;Database=SubscriptionManagementdb;User Id=admin;Password=sWRW8bdS3pyNwxA;TrustServerCertificate=True";
+        }
+    }
+}
---
+++ SubscriptionManagement.API.IntegrationTest/TestServerSetup.cs
@@ -0,0 +1,22 @@
+﻿//using Microsoft.AspNetCore.Hosting;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SubscriptionManagement.API.IntegrationTest
+//{
+//    [TestFixture]
+//    public class TestServerSetup
+//    {
+//        protected readonly HttpClient _client;
+
+//        public TestServerSetup()
+//        {
+//            var server = new TestServer(new WebHostBuilder()
+//                .UseStartup<Startup>());
+//            _client = server.CreateClient();
+//        }
+//    }
+//}
